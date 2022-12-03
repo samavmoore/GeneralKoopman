169cd1f8ca3357b765185dcf8d0472625d54e5a7
samavmoore <eroom.a.mas@gmail.com>
2022-12-03 12:51:36 -0500

    New dataloaders

--- ConfigData.py
+++ ConfigData.py
@@ -1,19 +1,18 @@
 
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from pytorch_lightning import LightningDataModule
 
 class CustomKoopmanDataset(Dataset):
 
-    def __init__(self, parameters, data_name):
+    def __init__(self, n_states, n_shifts, n_traj, traj_len, data_path):
+        super.__init__()
         # load orig_data
-        orig_data = np.load(data_name)
+        orig_data = np.load(data_path)
 
         # shift the data
-        shifted_data = shift_n_stack(orig_data, parameters)
-
-        n_states = parameters['n_states']
+        shifted_data = shift_n_stack(orig_data, n_shifts, n_traj, traj_len)
 
         self.states = shifted_data[0,0:n_states,:]
         self.context = shifted_data[0,n_states:,:]
@@ -27,15 +26,12 @@ class CustomKoopmanDataset(Dataset):
         return self.n_samples
 
 
-def shift_n_stack(data, params):
+def shift_n_stack(data, n_shifts, n_traj, traj_len):
     '''
         Purpose: Shift and stack the original data so that the state values at a later time (up to n_shifts later) 
-                are easily accessible during training.
+                are easily accessible during training for loss calculation.
     '''
 
-    n_shifts = params['n_shifts']
-    traj_len = params['traj_len']
-    n_traj = params['n_traj']
     data = data.astype('float32')
     dim0, dim1 = data.shape
     new_dim = (traj_len-n_shifts)*n_traj
@@ -82,3 +78,69 @@ class KoopmanDataModule(LightningDataModule):
         return DataLoader(self.koop_val, batch_size=self.batch_size)
 
 
+class ContextDataset(Dataset):
+    def __init__(self, data_path):
+        super().__init__()
+        self.data = np.load(data_path)
+
+        self.data = torch.from_numpy(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index,:]
+
+    def __len__(self):
+        return len(self.data[:,0])
+
+
+class ContextDataModule(LightningDataModule):
+    def __init__(self, data_path, batch_size):
+        super().__init__()
+
+        self.data_path = data_path
+        self.batch_size = batch_size
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            self.full_data = ContextDataset(self.data_path)
+            self.train, self.val = random_split(self.full_data, [.7, .3])
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
+        
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.batch_size)
+
+class EigenPretrainDataset(Dataset):
+    def __init__(self, data_path, n_states):
+        super().__init__()
+        self.data = np.load(data_path)
+
+        self.states = self.data[:,:n_states]
+        self.context = self.data[:,n_states:]
+
+        self.states = torch.from_numpy(self.states)
+        self.context = torch.from_numpy(self.context)
+
+    def __getitem__(self, index):
+        return self.states[index,:], self.context[index,:]
+
+    def __len__(self):
+        return len(self.data[:,0])
+
+class EigenPretrainModule(LightningDataModule):
+    def __init__(self, data_path, batch_size):
+        super().__init__()
+
+        self.data_path = data_path
+        self.batch_size = batch_size
+
+    def setup(self, stage: str):
+        if stage == 'fit':
+            self.full_data = EigenPretrainDataset(self.data_path)
+            self.train, self.val = random_split(self.full_data, [.7, .3])
+            
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.batch_size)
